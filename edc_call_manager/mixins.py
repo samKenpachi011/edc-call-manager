2e98484c555de9d8b06a1c42d3834b32aa2c556a
erikvw <ew2789@gmail.com>
2016-05-24 13:46:17 +0200

    add mixin for subject locator to update Log

---
+++ edc_call_manager/mixins.py
@@ -0,0 +1,41 @@
+from django.apps import apps as django_apps
+from django.db import models
+from django.core.exceptions import MultipleObjectsReturned
+
+
+class CallLogLocatorMixin(models.Model):
+
+    """A Locator model mixin that has the Locator model update the Log if changed."""
+
+    def save(self, *args, **kwargs):
+        self.update_call_log()
+        super(CallLogLocatorMixin, self).save(*args, **kwargs)
+
+    def get_call_log_model(self):
+        """If using the call_manager, return the Log model so it can be updated."""
+        try:
+            return django_apps.get_model('call_manager', 'log')
+        except LookupError:
+            return None
+
+    def get_call_log_options(self):
+        """If using the call_manager, return the Log model filter options."""
+        return dict(call__registered_subject=self.registered_subject)
+
+    def update_call_log(self):
+        """If using the call_manager, update the Log model otherwise do nothing."""
+        Log = self.get_call_log_model()
+        if Log:
+            try:
+                log = Log.objects.get(**self.get_call_log_options())
+                log.locator_information = self.to_string()
+                log.save()
+            except MultipleObjectsReturned:
+                for log in Log.objects.filter(**self.get_call_log_options()):
+                    log.locator_information = self.to_string()
+                    log.save()
+            except Log.DoesNotExist:
+                pass
+
+    class Meta:
+        abstract = True
