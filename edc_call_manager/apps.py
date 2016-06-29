9b062e74a4b5772d2193d648b13e29a53e8e3455
erikvw <ew2789@gmail.com>
2016-06-29 20:14:11 +0200

    correct AppConfig class name

--- edc_call_manager/apps.py
+++ edc_call_manager/apps.py
@@ -4,10 +4,11 @@ from django.apps import AppConfig
 from edc_call_manager.caller_site import site_model_callers
 
 
-class CallManagerAppConfig(AppConfig):
-    name = 'call_manager'
+class EdcCallManagerAppConfig(AppConfig):
+    name = 'edc_call_manager'
     verbose_name = 'Call Manager'
 
     def ready(self):
         sys.stdout.write('Loading {} ...\n'.format(self.verbose_name))
         site_model_callers.autodiscover()
+        sys.stdout.write(' Done loading {}.\n'.format(self.verbose_name))
