5bd916b2bfc85ebe23aa8da5a6a3003c23a04821
erikvw <ew2789@gmail.com>
2016-08-06 07:47:23 +0200

    PEP8

--- edc_call_manager/forms.py
+++ edc_call_manager/forms.py
@@ -20,15 +20,23 @@ class LogEntryForm(CrispyFormMixin, forms.ModelForm):
         cleaned_data = super(LogEntryForm, self).clean()
         log = cleaned_data.get('log')
         if cleaned_data.get('appt_location') == OTHER and not cleaned_data.get('appt_location_other'):
-            raise forms.ValidationError('You wrote the appointment location is OTHER, please specify below.')
+            raise forms.ValidationError(
+                'You wrote the appointment location is OTHER, please specify below.')
         if cleaned_data.get('appt') == YES and not cleaned_data.get('appt_date'):
-            raise forms.ValidationError('You wrote the participant is willing to make an appointment. Please specify the appointment date.')
+            raise forms.ValidationError(
+                'You wrote the participant is willing to make an appointment. '
+                'Please specify the appointment date.')
         if cleaned_data.get('appt') == YES and not cleaned_data.get('appt_grading'):
-            raise forms.ValidationError('You wrote the participant is willing to make an appointment. Please specify if this is a firm appointment date or not.')
+            raise forms.ValidationError(
+                'You wrote the participant is willing to make an appointment. '
+                'Please specify if this is a firm appointment date or not.')
         if cleaned_data.get('appt') == YES and not cleaned_data.get('appt_location'):
-            raise forms.ValidationError('You wrote the participant is willing to make an appointment. Please specify the appointment location.')
+            raise forms.ValidationError(
+                'You wrote the participant is willing to make an appointment. '
+                'Please specify the appointment location.')
         if log.call.call_status == CLOSED:
-            raise forms.ValidationError('This call is closed. You may not add to or change the call log.')
+            raise forms.ValidationError(
+                'This call is closed. You may not add to or change the call log.')
         return cleaned_data
 
     class Meta:
