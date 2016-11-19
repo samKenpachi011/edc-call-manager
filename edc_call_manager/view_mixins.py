b4117d8784927c9a815ae606b2c925e8b018cd69
erikvw <ew2789@gmail.com>
2016-11-19 22:24:52 +0200

    fix import

--- edc_call_manager/view_mixins.py
+++ edc_call_manager/view_mixins.py
@@ -7,7 +7,7 @@ from django.core.urlresolvers import reverse
 from django.utils.decorators import method_decorator
 
 from edc_base.utils.age import formatted_age
-from edc_base.views.edc_base_view_mixin import EdcBaseViewMixin
+from edc_base.view_mixins import EdcBaseViewMixin
 from edc_call_manager.constants import NO_CONTACT, INDIRECT_CONTACT, DIRECT_CONTACT
 from edc_constants.constants import ALIVE, CLOSED, NO
 
